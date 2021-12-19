be4390ac91f3de6ba5ff5e12ea9298ae7d31517c
Kader Oral <63172521+tutunamayanlar2021@users.noreply.github.com>
2021-12-20 00:18:20 +0300

    web
    dil desteği üzerinde çalışmalar devam ediyor. home/index te dil desteği sağlandı.(dün hasta olduğum için commit atmayi unuttum)

---
+++ OrnekPro/OrnekPro/OrnekPro/Entity/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrnekPro.Entity
+{
+   
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        [Required]
+        [MaxLength(500)]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Director { get; set; }
+        public string ImageUrl { get; set; }
+
+        [Required]
+        public int? TurId { get; set; }
+    }
+}
---
+++ OrnekPro/OrnekPro/OrnekPro/Models/HomePageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using OrnekPro.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrnekPro.Models
+{
+    public class HomePageViewModel
+    {
+        public List<Movie>PopularMovies{ get; set; }
+    }
+}
